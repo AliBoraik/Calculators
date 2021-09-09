2e3c6617e2bf473fe92774d4f011fd83125383b5
Ali Boraik <aliaqel2021@gmail.com>
2021-09-09 23:15:48 +0300

    add test

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("from test branch");
+            Console.WriteLine("from test branch");
+            Console.WriteLine("from test branch");
+        }
+    }
+}
