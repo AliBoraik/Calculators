40afc57228f78f3c0e7c0f2ca378a2756bdb9059
Ali Boraik <aliaqel2021@gmail.com>
2021-09-10 19:30:03 +0300

    add Lab1 and operation by class BigInteger to get largest result

---
+++ Lab1/Calculater/Calculater/MyCalculater.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Numerics;
+
+namespace Calculater
+{
+    class MyCalculater
+    {
+        public MyCalculater()
+        {
+            // I'm using class BigInterger to get result without errors
+            // (:
+        }
+
+
+        public BigInteger add(string s1, string s2)
+        {
+            return BigInteger.Add(BigInteger.Parse(s1), BigInteger.Parse(s2));
+        }
+        public BigInteger Subtract(string s1, string s2)
+        {
+            return BigInteger.Subtract(BigInteger.Parse(s1), BigInteger.Parse(s2));
+        }
+        public BigInteger Multiply(string s1, string s2)
+        {
+            return BigInteger.Multiply(BigInteger.Parse(s1), BigInteger.Parse(s2));
+        }
+        public BigInteger Divide(string s1, string s2)
+        {
+            return BigInteger.Divide(BigInteger.Parse(s1), BigInteger.Parse(s2));
+        }
+
+
+        public bool isNumbers(string n1, string n2)
+        {
+            BigInteger number_1;
+            if (BigInteger.TryParse(n1, out number_1))
+            {
+                if (BigInteger.TryParse(n2, out number_1))
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("The second orgument is not number!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The first orgument is not number!!");
+            }
+            return false;
+        }
+        public void proces(string n1, string oper, string n2)
+        {
+            var number_1 = n1;
+            var number_2 = n2;
+            switch (oper)
+            {
+                case "+":
+                    Console.WriteLine(add(number_1, number_2).ToString());
+                    break;
+                case "-":
+                    Console.WriteLine(Subtract(number_1, number_2).ToString());
+                    break;
+                case "*":
+                    Console.WriteLine(Multiply(number_1, number_2).ToString());
+                    break;
+                case "/":
+                    Console.WriteLine(Divide(number_1, number_2).ToString());
+                    break;
+
+                default:
+                    Console.WriteLine("operation is not correct!!");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Lab1/Calculater/Calculater/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 3)
+            {
+                string numbver_1 ;
+                string numbver_2 ;
+                string operation ;
+
+                MyCalculater myCalcuater = new MyCalculater();
+
+                numbver_1 = args[0];
+                operation = args[1];
+                numbver_2 = args[2];
+
+                if (myCalcuater.isNumbers(numbver_1, numbver_2))
+                {
+                    myCalcuater.proces(numbver_1, operation, numbver_2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("неверное количество аргументов");
+            }
+        }
+    }
+}
