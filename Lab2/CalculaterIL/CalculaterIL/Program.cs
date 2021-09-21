9947755c709cd8603aa87dec96e335f8292becd2
Ali <ali99aqel@gmail.com>
2021-09-21 19:42:17 +0300

    add Lab2 (calculatorIL)

---
+++ Lab2/CalculaterIL/CalculaterIL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CalculaterIL
+{
+    public class  Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                string numbver_1;
+                string numbver_2;
+                string operation;
+                
+                numbver_1 = args[0];
+                operation = args[1];
+                numbver_2 = args[2];
+
+                if (IsNumbers(numbver_1, numbver_2))
+                {
+                    if (IsOperation(operation))
+                    {
+                        Console.WriteLine(Proces(numbver_1, operation, numbver_2));
+                        return 0;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("неверное количество аргументов");
+            }
+            return 1;
+        }
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        private static extern bool IsOperation(string oper);
+        
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        private static extern bool IsNumbers(string num1,string num2);
+        
+        [MethodImpl(MethodImplOptions.ForwardRef)]
+        private static extern string Proces(string num1,string oper,string num2);
+        
+    }
+}
---
+++ Lab2/CalculaterIL/TestProject1/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(new []{"3"+"+"+"3"},"0")]
+        [InlineData(new []{"3"+"-"+"3"},"0")]
+        [InlineData(new []{"3"+"*"+"3"},"0")]
+        [InlineData(new []{"3"+"/"+"3"},"0")]
+        public static void TestMain_Result_Main(string[] args,int restult)
+        {
+            Assert.Equal(24,CalculaterIL.Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new []{"3"+"&"+"3"},"1")]
+        [InlineData(new []{"dfg"+"-"+"3"},"1")]
+        [InlineData(new []{"3"+"*"+"hbgvf"},"1")]
+        [InlineData(new []{"3"+" "+"3"},"1")]
+        public static void TestMain_ReturmOne(string[] args,int restult)
+        {
+            Assert.Equal(24,CalculaterIL.Program.Main(args));
+        }
+    }
+}
