8e479ce783b52c723490940396e5d75e667ae9f3
Ali <ali99aqel@gmail.com>
2021-10-22 19:20:02 +0300

    add Handler file to connect to the pages.

---
+++ Lab5/WebApplication/CalculatorConstants.fs
@@ -0,0 +1,39 @@
+﻿namespace CalculatorF
+
+
+module public CalculatorConstants =
+    [<Literal>]
+    let Addition = "add"
+
+    [<Literal>]
+    let Subtract = "sub"
+
+    [<Literal>]
+    let Multiply = "mul"
+
+    [<Literal>]
+    let Divide = "div"
+
+    [<Literal>]
+    let NotIntegerErrorMassage = "Argument is not numbers or so long..."
+
+    [<Literal>]
+    let DivideByZero = "Attempted to divide by zero."
+
+    [<Literal>]
+    let OperationErrorMassage = "Operation is not correct!!"
+
+    [<Literal>]
+    let ArgumentErrorMassage = "неверное количество аргументов !!"
+
+    [<Literal>]
+    let Ok_Code = 0
+
+    [<Literal>]
+    let OperationErrorCode = 1
+
+    [<Literal>]
+    let NumbersErrorCode = 2
+
+    [<Literal>]
+    let ArgumentErrorCode = 3
---
+++ Lab5/WebApplication/Handlers/Handler.fs
@@ -0,0 +1,15 @@
+﻿module Handler
+
+open System
+open Giraffe
+
+
+let resultHandler (result: Decimal) : HttpHandler =
+    let result = result.ToString("G29")
+
+    setStatusCode 200 >=> text $"Result = {result}"
+
+
+let errorHandler (erroeMassege: String) : HttpHandler =
+    setStatusCode 404
+    >=> text $"sorry something is wrong try aging! error {erroeMassege}"
