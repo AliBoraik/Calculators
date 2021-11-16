79de7fac701916580971cb04b6cf0e7aea574e00
Ali <ali99aqel@gmail.com>
2021-11-16 05:44:26 +0300

    Add all files :)

---
+++ Lab7/Claculator_MVC/Calculator_MVC/Calculator_MVC/Models/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator_MVC.Models
+{
+    public class Result
+    {
+        public string num1 { get; set; }
+        public string opre { get; set; }
+        public string num2 { get; set; }
+    }
+}
