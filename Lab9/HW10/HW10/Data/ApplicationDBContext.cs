83db185aa83bcf21ac7cbace0a9d3e28faebf7ce
Ali <ali99aqel@gmail.com>
2021-12-01 22:06:11 +0300

    add dbContext

---
+++ Lab9/HW10/HW10/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using HW10.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HW10.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
