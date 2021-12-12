68b3523554506f9ffe1ed6041533c598609d0a5b
Ali <ali99aqel@gmail.com>
2021-12-13 00:31:46 +0300

    add controllers and views

---
+++ Lab10/HW11/HW11/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HW11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
